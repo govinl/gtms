0319b9ccf68584d7520830b179f3456ebd4e1195
feng-wild <wilddata@foxmail.com>
2019-12-11 14:50:21 +0800

    页面模板

--- src/main/resources/static/js/own/login.js
+++ src/main/resources/static/js/own/login.js
@@ -45,7 +45,7 @@ function login() {
                                 setCookie('userLevel', json2.data.plevel, 1);
                                 window.location.href = json2.url;
                             } else {
-                                layer.msg(json2.msg,{icon:1});
+                                layer.msg(json2.msg,{icon:2});
                             }
                         }
                     });
@@ -73,7 +73,7 @@ function getCookie(cookie_name)
     if (cookie_pos != -1)
     {
         // 把cookie_pos放在值的开始，只要给值加1即可。
-        cookie_pos += cookie_name.length + 1;      //这里容易出问题，所以请大家参考的时候自己好好研究一下
+        cookie_pos += cookie_name.length + 1;
         var cookie_end = allcookies.indexOf(";", cookie_pos);
 
         if (cookie_end == -1)
@@ -81,7 +81,7 @@ function getCookie(cookie_name)
             cookie_end = allcookies.length;
         }
 
-        var value = unescape(allcookies.substring(cookie_pos, cookie_end));         //这里就可以得到你想要的cookie的值了。。。
+        var value = unescape(allcookies.substring(cookie_pos, cookie_end));
     }
     return value;
 }
